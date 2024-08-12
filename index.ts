105b285068504ab5a8d32cd6c5b259af51957d44
winnyDiese <winnydiese@gmail.com>
2024-08-12 12:32:44 +0200

    lsdkfjvnlkub

--- index.ts
+++ index.ts
@@ -39,7 +39,43 @@ class Person {
 }
 
 let person = new Person("Paul",40)
-person.greet()
+// person.greet()
 
 
+// Interface : Definir l'interface
+interface Student{
+    name:string
+    age:number
+    grade:string
+}
+
+let student: Student = {name:'Pascal', age:25, grade:'Graduate'}
+// console.log(student)
+
+// Interface : using interface for define interface in function
+function printStudent(student: Student):void{
+    console.log(`${student.name} is ${student.age} years old and is in grade ${student.grade}`)
+}
+
+// printStudent(student)
+
+
+// TYPE AVANCEE
+// union type
+let id: number|string
+id = 30
+id = "Xvftgkola44"
+
+
+// Intersection type
+interface Employee {
+    employeeId: number
+}
+
+interface Manager extends Employee {
+    departement: string
+}
+
+let manager: Manager = {employeeId: 101 ,departement: 'HR'}
+console.log(manager)
 
